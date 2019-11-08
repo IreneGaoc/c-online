904e191f290e7e37e2e718a49b0dd52901365037
IreneGaoc <irene.gao@eecol.com>
2019-11-08 13:50:35 -0700

    firebase and sign up

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,8 +12,31 @@ const config =  {
     appId: "1:750805443468:web:d6819332ec10b73faaaca9",
     measurementId: "G-RJW4K8BST0"
   };
-
   firebase.initializeApp(config);
+
+  export const createUserProfileDocument = async (userAuth, additionalData) =>{
+      if (!userAuth) return;
+
+      const userRef = firestore.doc(`users/${userAuth.uid}`);
+      const snapShot = await userRef.get();
+      if(!snapShot.exists){
+          const {displayName, email} = userAuth;
+          const createdAt = new Date();
+          try{
+              await userRef.set({
+                  displayName,
+                  email,
+                  createdAt,
+                  ...additionalData
+              })
+          }catch(error){
+              console.log('error creating user', error.message);
+          }
+      }
+      return userRef;
+  };
+  
+
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
